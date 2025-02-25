99a22d0ddeda55e99caa6b2151eee9fb33a08059
MRFauzy <fauzyrandi5@gmail.com>
2025-02-25 16:03:32 +0700

    menu

--- lib/screens/profil_screen.dart
+++ lib/screens/profil_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+class ProfilScreen extends StatelessWidget {
+  const ProfilScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
